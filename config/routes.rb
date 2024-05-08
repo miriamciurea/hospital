91e70a30684415bdd8a27f361ced8d7ee2affa39
Miriam Ciurea <miriamciurea777@icloud.com>
2024-05-08 11:13:41 +0300

    routes are looking better

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
   delete '/patients/:id', to: 'patients#destroy', as: 'delete_patient'
   resources :patients, only: [:show, :index, :new, :create, :edit, :update]
   resources :users, only: [:index, :show] do
+    resources :specializations, only: [:show]
     resources :patients, only: [:show] do
       resources :appointments, only: [:new, :create, :edit, :update]
     end
   end
+  resources :appointments, only: [:destroy, :index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
